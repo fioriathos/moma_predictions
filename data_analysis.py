96149f0a5579f1067b9f881670948fe523eb5a46
fioriathos <40672529+fioriathos@users.noreply.github.com>
2020-06-09 13:34:08 +0200

    final data analysis and bugs in recursive lengths

--- data_analysis.py
+++ data_analysis.py
@@ -1,130 +1,242 @@
+import numpy as np
+import pandas as pd
+import recursive_lengths as rl
+import seaborn as sns
+from scipy.stats import linregress
+from scipy import stats
+import matplotlib.pyplot as plt
 ##### Some functions useful for data analysis
-def load_and_filer(pwd):
+def load_and_filer(pwd,rval=0.95):
+    """load csv filter div and 0.95 r value and at least 2 clls"""
     df = pd.read_csv(pwd)
     df = rl.give_good_structure(df)
-    df = df.groupby('cell').filter(lambda x: len(x)>2)
-    df = df.loc[df['end_type']=='DIVISION']
-    df =df.groupby('cell').filter(lambda x: linregress(x['frame'],np.log(x['length_box'])).rvalue>0.95)
-    df['time_sec'] = df['frame']*60*3
-    df['length_box_um'] = df['length_box']*0.065
+    df = df.loc[(df['end_type']=='DIVISION')|(df['end_type']=='DIV')|(df['end_type']=='div')]
+    if 'length_box' in df.columns: #guillaume data
+        df['time_sec'] = df['frame']*60*3
+        df['length_box_um'] = df['length_box']*0.065
+    else:
+        df['length_box_um'] = (df['vertical_bottom'] - df['vertical_top'])*0.065
+    df = df.groupby('cell').filter(lambda x: True if len(x['length_box_um'])>2 else False)
+    df =df.groupby('cell').filter(lambda x: linregress(x['time_sec'],np.log(x['length_box_um'])).rvalue>rval)
     #df = rl.give_unique_dataset(df,6,18)
-    df =df[['length_box_um','time_sec','parent_id','id','gl','date','pos','cell','lane_ID','length_box','end_type']]
+    df =df[['length_box_um','time_sec','parent_id','id','gl','date','pos','cell','lane_ID','end_type']]
     return df
 def predict(df,x):
+    """Predict length and growht rate using inferred parameters"""
     ml,g,sl2,sm2,sd2= x
     _,in_dic = rl.build_data_strucutre(df,'length_box_um',1)
     pred_mat = rl.prediction_total(ml,g,sl2,sm2,in_dic['reind_v'],in_dic['dat_v'],in_dic['s'],in_dic['S'],in_dic['dt'],in_dic['lane_ID_v'],in_dic['val_v'],1,sd2,nproc=10)
     df = rl.merge_df_pred(df,pred_mat)
     return df
-def useful_quantities(dffin):
+def panda_files_descriptions(df):
+    """Some simple statistics of the panda file"""
+    division_time = (df.groupby('cell')['time_sec'].last()-df.groupby('cell')['time_sec'].first()).mean()/60
+    number_cells = len(df.cell.unique())
+    corr = rl.long_corr_in_data(df,'length_box_um',2)
+    inilen = df.groupby('cell')['length_box_um'].first().mean()
+    divlen = df.groupby('cell')['length_box_um'].last().mean()
+    elrat = df.groupby('cell').apply(lambda x:\
+                                     linregress(x['time_sec'],np.log(x['length_box_um'])).slope).mean()*60
+    ac_ti =\
+    (df.groupby('cell')['time_sec'].nth(1)-df.groupby('cell')['time_sec'].nth(0)).mean()/60
+    return {'ac_ti':ac_ti,'length_before_division':divlen,'length_after_division':inilen,\
+            'cv_el_rat':corr[1],'corr_1_gen':corr[0][0,0],'corr_2_gen':corr[0][1,0],'division_time':division_time,'number_cells':number_cells,'elrat':elrat}
+def usefulquantities(dffin):
+    """Some interesting variable to define on dffin (the already predicted
+    paths) """
     dffin['log_length_box'] = np.log(dffin['length_box_um'])
     dffin['time_min']=dffin['time_sec']/60
-    dffin['cell_cycle'] = dffin.groupby('cell').apply(lambda x: (x['time_sec']-x['time_sec'].iloc[0])/(x['time_sec'].iloc[-1]-x['time_sec'].iloc[0]) ).values
     dffin['pred_length_box_um'] = np.exp(dffin['pred_log_length'])
-    dffin['added_length'] = dffin.groupby('cell').apply(lambda x: x['pred_length_box_um']-x['pred_length_box_um'].iloc[0] ).values
-    return dffin
-def binnedcorr(df,variable,bins):
+    dffin['unique_id'] = dffin['cell']+dffin['time_sec'].apply(lambda x:str(x))
+    dffin['cv_gr']= dffin.groupby('cell')['pred_growth_rate'].transform(lambda x:\
+                                                                  np.std(x)/np.mean(x))
+    dffin['std_gr']= dffin.groupby('cell')['pred_growth_rate'].transform(lambda x: np.std(x))
+    dffin['mean_gr'] = dffin.groupby('cell')['pred_growth_rate'].transform(lambda x: np.mean(x))
+    dffin['mean_len'] = dffin.groupby('cell')['pred_length_box_um'].transform(lambda x: np.mean(x))
+    dffin['norm_pred_growth_rate'] = (dffin['pred_growth_rate']-dffin.groupby('cell')['pred_growth_rate'].transform(lambda\
+    x: np.mean(x)))/dffin.groupby('cell')['pred_growth_rate'].transform(lambda x: np.mean(x))
+    dffin = rl.genalogy(dffin,'parent_cell') #Create genealogy
+    dffin = rl.genalogy(dffin,'g_parent_cell')
+    dffin = rl.genalogy(dffin,'g_g_parent_cell')
+    dffin = dffin.set_index('unique_id')
+    qq= dffin.groupby('cell').apply(lambda x: (x['pred_length_box_um']-x['pred_length_box_um'].iloc[0])/(x['pred_length_box_um'].iloc[-1]-x['pred_length_box_um'].iloc[0])).rename('add_len')
+    jj= dffin.groupby('cell').apply(lambda x: (x['time_sec']-x['time_sec'].iloc[0])/(x['time_sec'].iloc[-1]-x['time_sec'].iloc[0])).rename('cell_cycle')
+    return pd.concat([dffin, qq.reset_index().set_index('unique_id')['add_len'], jj.reset_index().set_index('unique_id')['cell_cycle']], axis=1, join='inner')
+
+def binnedcorr(df,variable,bins,var_y='pred_growth_rate'):
     """Bin the variable and compute mean+std+number of points for growth rate """
     le = np.linspace(df['{}'.format(variable)].min(),df['{}'.format(variable)].max(),bins+1)
     bins = pd.IntervalIndex.from_tuples([(i,j) for i,j in zip(le[:-1],le[1:])])
     df['bin_var'] = pd.cut(df['{}'.format(variable)], bins)
-    gro = df.groupby('bin_var').apply(lambda x: x['pred_growth_rate'])
+    gro = df.groupby('bin_var').apply(lambda x: x['{}'.format(var_y)])
     #return gro.mean(),gro.std(),gro.size()
     gro = gro.reset_index()
     gro['bin_var'] =  gro['bin_var'].astype('str')
     gro['bin_var'] = gro['bin_var'].apply(lambda x:'('+';'.join(['{:.2f}'.format(float(i)) for  i in x.replace('(','').replace('[','').replace(']','').replace(')','').split(",")])+')')
-    return  gro[['bin_var','pred_growth_rate']].rename(columns={'bin_var':'bin_{}'.format(variable)}) 
+    return  gro[['bin_var','{}'.format(var_y)]].rename(columns={'bin_var':'bin_{}'.format(variable)})
 def at_birth(df,variable,npoint):
     """Variable at birth and elongation rate mean over npoint"""
     return df.groupby('cell')[['{}'.format('{}'.format(variable)),'pred_growth_rate']].apply(lambda x: x.head(npoint).mean()).rename(columns={'pred_length_box_um':'{}_at_birth'.format(variable)})
-def autocorrelation(df,maxt,step,vari='pred_growth_rate',acquisiton_time=3):
+def connect_cells(dfte,vari):
+    """Connect cells between genealogies and return dataframe with super_cell id and variable"""
+    # Create the variabel cell for mother, grand mother and grand grand mother
+    if 'g_parent_cell' not in dfte.columns:
+        dfte = rl.genalogy(dfte,'parent_cell') #Create genealogy
+    if 'g_g_parent_cell' not in dfte.columns:
+        dfte = rl.genalogy(dfte,'g_parent_cell')
+    if 'g_g_g_parent_cell' not in dfte.columns:
+        dfte = rl.genalogy(dfte,'g_g_parent_cell')
+    #give unique index to all cells
+    dfte['uid'] = dfte['cell']+dfte['time_sec'].apply(lambda x: str(x))
+    vac=[];sc=[];uid = []
+    # Create a vecotor for the variable of interest of cell,mother,grand mother and grand grand mother and an unique identifier of it
+    for c,idx in enumerate(dfte['cell'].unique()):
+        dau = dfte.loc[dfte['cell']==idx]
+        pc = dau['parent_cell'].iloc[0]
+        mum = dfte.loc[dfte['cell']==pc]
+        gpc = dau['g_parent_cell'].iloc[0]
+        gmum = dfte.loc[dfte['cell']==gpc]
+        ggpc = dau['g_g_parent_cell'].iloc[0]
+        ggmum = dfte.loc[dfte['cell']==ggpc]
+        gggpc = dau['g_g_g_parent_cell'].iloc[0]
+        gggmum = dfte.loc[dfte['cell']==gggpc]
+        fte = lambda x: x[['{}'.format(vari),'uid']].values
+        tmp = np.vstack([fte(gggmum),fte(ggmum),fte(gmum),fte(mum),fte(dau)])
+        vac.append(tmp[:,0])
+        uid.append(tmp[:,1])
+        sc.append(['super_cell_{}'.format(c)]*len(tmp))
+    return pd.DataFrame({'super_cell':np.hstack(sc),'uid':np.hstack(uid),'{}'.format(vari):np.hstack(vac)})
+def correlation(dffin,Dt,vari):
+    """Correlation function of <vari,vari_dt> """
+    if Dt==0:return np.array([1,0])
+    rigth = dffin.groupby('super_cell').apply(lambda x: x[['{}'.format(vari),'uid']].values[Dt:,:]).values
+    left = dffin.groupby('super_cell').apply(lambda x: x[['{}'.format(vari),'uid']].values[:-Dt,:]).values
+    tmp = pd.DataFrame(data=np.hstack((np.vstack(left),np.vstack(rigth))))
+    #find unique index
+    tmp = tmp.drop_duplicates()
+    per_r = ((tmp.loc[:,0]*tmp.loc[:,2]).mean()-tmp.loc[:,0].mean()*tmp.loc[:,2].mean())/(tmp.loc[:,0].std()*tmp.loc[:,2].std())
+    err_r = np.sqrt((1-per_r**2)/(tmp.shape[0]-2))
+    return np.array([per_r,err_r])
+def autocorrelation(df,maxt,step,vari,acquisiton_time,division_time):
     """Find autocorrelation even between genalogy frmo t=0 to t=maxt with step steps"""
-    def connect_cells(dfte,vari):
-        """Connect cells between genealogies and return dataframe with super_cell id and variable"""
-        # Create the variabel cell for mother, grand mother and grand grand mother
-        if 'g_parent_cell' not in dfte.columns:
-            dfte = rl.genalogy(dfte,'parent_cell') #Create genealogy
-            dfte = rl.genalogy(dfte,'g_parent_cell')
-        vac=[];sc=[];uid = []
-        # Create a vecotor for the variable of interest of cell,mother,grand mother and grand grand mother and an unique identifier of it
-        for c,idx in enumerate(dfte['cell'].unique()):
-            dau = dfte.loc[dfte['cell']==idx]
-            pc = dau['parent_cell'].iloc[0]
-            mum = dfte.loc[dfte['cell']==pc]
-            gpc = dau['g_parent_cell'].iloc[0]
-            gmum = dfte.loc[dfte['cell']==gpc]
-            ggpc = dau['g_g_parent_cell'].iloc[0]
-            ggmum = dfte.loc[dfte['cell']==ggpc]
-            tmp = np.hstack([dau['{}'.format(vari)].values,mum['{}'.format(vari)].values,gmum['{}'.format(vari)].values,ggmum['{}'.format(vari)].values])
-            tmp1 = np.hstack([ggmum['time_sec'].apply(lambda x:str(x)+'_'+str(ggpc)).values,gmum['time_sec'].apply(lambda x:str(x)+'_'+str(gpc)).values,mum['time_sec'].apply(lambda x:str(x)+'_'+str(pc)).values,dau['time_sec'].apply(lambda x:str(x)+'_'+str(idx)).values])
-            vac.append(tmp)
-            uid.append(tmp1)
-            sc.append(['super_cell_{}'.format(c)]*len(tmp))
-        return pd.DataFrame({'super_cell':np.hstack(sc),'uid':np.hstack(uid),'{}'.format(vari):np.hstack(vac)})
-    def correlation(dffin,Dt,vari):
-        """Correlation function of <vari,vari_dt> """
-        if Dt==0:return 1,1e10
-        rigth = dffin.groupby('super_cell').apply(lambda x: x[['{}'.format(vari),'uid']].values[Dt:,:]).values
-        left = dffin.groupby('super_cell').apply(lambda x: x[['{}'.format(vari),'uid']].values[:-Dt,:]).values
-        tmp = np.hstack((np.vstack(left),np.vstack(rigth)))
-        #find unique index
-        #return tmp
-        _,ind = np.unique(tmp[:,1:2]+tmp[:,3:4],return_index=True) 
-        # Unique index array
-        tmp = tmp[ind,:]
-        left = tmp[:,0:1]; rigth = tmp[:,2:3]
-        sl = np.std(left);sr = np.std(rigth)
-        ml = np.mean(left);mr = np.mean(rigth)
-        ct = np.mean(left*rigth)
-        return (ct-ml*mr)/(sl*sr),left.shape[0]
+    maxt = int(maxt/acquisiton_time)
+    step = int(step/acquisiton_time)
     df = connect_cells(df,vari)
-    #for Dt in np.arange(0,maxt,step):
-    #    print(Dt)
-    #    print(correlation(df,Dt,vari))
-    return np.array([correlation(df,Dt,vari) for Dt in np.arange(0,maxt,step)]), np.arange(0,maxt,step)*acquisiton_time
+    return np.vstack([correlation(df,Dt,vari) for Dt in\
+                      np.arange(0,maxt,step)]),\
+        np.arange(0,maxt,step)*acquisiton_time/division_time
+def muther_dau_gdau(df,var_m,var_d,var_gd):
+    """Find the variables between mum daug gdaug"""
+    if 'g_parent_cell' not in df.columns:
+        df = rl.genalogy(df,'parent_cell') #Create genealogy
+    tmp={'gr_mu_{}'.format(var_m):[],'mu_{}'.format(var_d):[],'daugther_{}'.format(var_gd):[]}
+    for k in df.cell.unique():
+        dau = df.loc[df['cell']==k]
+        dau_var = dau['{}'.format(var_gd)].iloc[0]
+        nid = dau.parent_cell.iloc[0]
+        mu = df.loc[df['cell']==nid]
+        try:#if mother exists
+            mu_var = mu['{}'.format(var_d)].iloc[0]
+            nid = mu.g_parent_cell.iloc[0]
+            mu_var = mu['{}'.format(var_d)].iloc[0]
+            gmu = df.loc[df['cell']==nid]
+        except IndexError:
+            continue
+        try:# if grand mother exists
+            tmp['gr_mu_{}'.format(var_m)].append(gmu['{}'.format(var_m)].iloc[0])
+            tmp['mu_{}'.format(var_d)].append(mu_var)
+            tmp['daugther_{}'.format(var_gd)].append(dau_var)
+        except IndexError:
+            tmp['gr_mu_{}'.format(var_m)].append(np.nan)
+            tmp['mu_{}'.format(var_d)].append(mu_var)
+            tmp['daugther_{}'.format(var_gd)].append(dau_var)
+    return pd.DataFrame(tmp)
+def qq_plot(obs,var,fname):
+    """qq plot with normal dist"""
+    plt.figure()
+    z = (obs-np.mean(obs))/np.std(obs)
+    stats.probplot(z, dist="norm", plot=plt)
+    plt.plot(np.arange(-3,3),np.arange(-3,3))
+    plt.xlim([-3,3])
+    plt.ylim([-3,3])
+    plt.title("Normal Q-Q plot {} in {}".format(var,fname))
+    plt.savefig("qq_{}".format(var))
+def\
+plot_corr(df,var_x,var_y,one_per_cell,kind='reg',addsave='',fname='',ret=False):
+    """Select 1 number per cell and plot the result"""
+    if one_per_cell:
+        ff =\
+        df.dropna().groupby('cell')[['{}'.format(var_x),'{}'.format(var_y)]].first()
+    else: ff=df
+    g = sns.jointplot(x="{}".format(var_x), y="{}".format(var_y),\
+                      data=ff,kind=kind)
+    g.plot_joint(plt.scatter, c="w", s=0.1, marker="o")
+    if kind=='reg':
+        g.annotate(stats.pearsonr)
+    g.fig.subplots_adjust(top=0.9)
+    g.fig.suptitle("Corr between {} and {} {} for\n {}".format(var_x,var_y,addsave,fname))
+    g.savefig("Corr_between_{}_and_{}_{}.png".format(var_x,var_y,addsave),bbox_inches='tight')
+    if ret: return ff
+def plot_autocorrelation(df,pa_fi_de):
+    divt = pa_fi_de['division_time']
+    act = pa_fi_de['ac_ti']
+    tmp_dat =\
+    autocorrelation(df,int(3*divt),step=act*2,vari='pred_growth_rate',acquisiton_time=act,division_time=divt)
+    plt.figure()
+    plt.plot(tmp_dat[1],tmp_dat[0][:,0])
+    plt.xlabel('time (doubling)')
+    plt.title("Autocorrelation for growth rate in {}".format(fname))
+    plt.savefig("autocorrelation.png")
+    tmp_dat = np.hstack([tmp_dat[0],tmp_dat[1][:,None]])
+    np.save('autocorrelation_{}'.format(fname),tmp_dat)
 
-def plot_figures(df,medium):
-    # Cell cycle
-    tmp = binnedcorr(df,'cell_cycle',10)
-    g = sns.catplot( x="bin_cell_cycle", y="pred_growth_rate",kind='box', data=tmp,height=10)
-    g.set_xticklabels(rotation=45)
-    g.savefig("{}_cell_cycle.png".format(medium))
-    #Length
-    tmp = binnedcorr(df,'pred_length_box_um',20)
-    g = sns.catplot( x="bin_pred_length_box_um", y="pred_growth_rate",kind='box', data=tmp,height=10)
-    g.set_xticklabels(rotation=45)
-    g.savefig("{}_length_box_um.png".format(medium))
-    dfilt = df.loc[(df['pred_length_box_um']>=df.groupby('cell')['pred_length_box_um'].first().mean())&(df['pred_length_box_um']<=df.groupby('cell')['pred_length_box_um'].last().mean())]
-    tmp = binnedcorr(dfilt,'pred_length_box_um',10)
-    g = sns.catplot( x="bin_pred_length_box_um", y="pred_growth_rate",kind='box', data=tmp,height=10)
-    g.set_xticklabels(rotation=45) 
-    g.savefig("{}_length_box_um_zoom.png".format(medium))
-    g.set(ylim=(0, 0.015))
-    #Added length
-    tmp = binnedcorr(df,'added_length',20)
-    g = sns.catplot( x="bin_added_length", y="pred_growth_rate",kind='box', data=tmp,height=10)
-    g.set_xticklabels(rotation=45)
-    g.savefig("{}_added_length.png".format(medium))
-    dfilt = df.loc[(df['added_length']>=0)&(df['added_length']<=df['added_length'].mean()+2*df['added_length'].std())]
-    tmp = binnedcorr(dfilt,'added_length',10)
-    g = sns.catplot( x="bin_added_length", y="pred_growth_rate",kind='box', data=tmp,height=10)
-    g.set_xticklabels(rotation=45)
-    g.savefig("{}_added_length_zoom.png".format(medium))
-    g.set(ylim=(0, 0.015))
-    # LEngth at birth
-    dfi = at_birth(df,'pred_length_box_um',5)
-    tmp = binnedcorr(dfi,'pred_length_box_um_at_birth',20)
-    g = sns.catplot( x="bin_pred_length_box_um_at_birth", y="pred_growth_rate",kind='box', data=tmp,height=10)
-    g.set_xticklabels(rotation=45)
-    g.savefig("{}_length_at_birth.png".format(medium))
-    me=dfi['pred_length_box_um_at_birth'].mean()
-    st=dfi['pred_length_box_um_at_birth'].std()
-    dfilt = dfi.loc[(dfi['pred_length_box_um_at_birth']>=me-2*st)&(dfi['pred_length_box_um_at_birth']<=me+2*st)]
-    tmp = binnedcorr(dfilt,'pred_length_box_um_at_birth',10)
-    g = sns.catplot( x="bin_pred_length_box_um_at_birth", y="pred_growth_rate",kind='box', data=tmp,height=10)
-    g.set_xticklabels(rotation=45)
-    g.savefig("{}_length_at_birth_zoom.png".format(medium))
-
-
+if __name__=="__main__":
+    import sys
+    import json
+    import pickle
+    file_name = sys.argv[1]
+    inf_par = sys.argv[2] #pikle
+    fname = file_name.replace('.csv','')
+    if sys.argv[3] == 'N':
+        df_ = load_and_filer(file_name)
+        #Inferred param
+        with open(inf_par,'rb') as f:
+            param = pickle.load(f)
+        df = predict(df_,param['param'])
+        df = usefulquantities(df)
+        #Description
+        print(param)
+        df.to_csv(file_name.replace('.csv','')+'_pred.csv')
+    else:
+        print("Prediction file loaded")
+        df = pd.read_csv(file_name.replace('.csv','')+'_pred.csv')
+        df['rvalue'] = df.groupby('cell')['length_box_um'].transform(lambda x: linregress(range(len(x)),x).rvalue)
+    pa_fi_de  =panda_files_descriptions(df)
+#    with open('file_description.txt', 'w') as file:
+#             file.write(json.dumps(pa_fi_de)) # use `json.loads` to do the
+#    # Is the growth rate normal distributed?
+    qq_plot(df['pred_growth_rate'],'growth_rate',fname=fname)
+#    # Is the over/under the mean normal distributed?
+    qq_plot(df['norm_pred_growth_rate'],'norm_growth_rate',fname=fname)
+#    #Autocorrelation function
+    plot_autocorrelation(df,pa_fi_de)
+    # Who correlates with whom?
+#    plot_corr(df,'mean_len','mean_gr',True,fname=fname)
+#    plot_corr(df,'mean_len','cv_gr',True,fname=fname)
+#    plot_corr(df,'mean_gr','cv_gr',True,fname=fname)
+#    plot_corr(df,'pred_length_box_um','norm_pred_growth_rate',True,'reg','at_birth',fname=fname)
+#    plot_corr(df,'cell_cycle','norm_pred_growth_rate',False,'scatter',fname=fname)
+#    plot_corr(df,'add_len','norm_pred_growth_rate',False,fname=fname)
+#    plot_corr(df,'pred_length_box_um','norm_pred_growth_rate',False,fname=fname)
+#    #Between muther and daugther
+#    tmp_dat = muther_dau_gdau(df,'cv_gr','cv_gr','cv_gr')
+#    plot_corr(tmp_dat,'mu_cv_gr','daugther_cv_gr',False,fname=fname)
+#    plot_corr(tmp_dat,'gr_mu_cv_gr','daugther_cv_gr',False,fname=fname)
+#    tmp_dat = muther_dau_gdau(df,'mean_gr','mean_gr','mean_gr')
+#    plot_corr(tmp_dat,'mu_mean_gr','daugther_mean_gr',False,fname=fname)
+#    plot_corr(tmp_dat,'gr_mu_mean_gr','daugther_mean_gr',False,fname=fname)
+#    plot_corr(df,'mean_gr','rvalue',True,fname=fname)
+#    plot_corr(df,'mean_len','rvalue',True,fname=fname)
+#    plot_corr(df,'mean_len','cv_gr',True,fname=fname)
 
 
--- recursive_lengths.py
+++ recursive_lengths.py
@@ -896,8 +896,8 @@ def denoised_dataset(df,step,nump=12):
 ############################### ADDITIONAL FUNCTIONS FOR ANALYSIS  #############################
 ################################################################################################
 def give_good_structure(df):
-    """Connect all pandas in files.txt togetehr"""
-    df['date'] = df['date'].apply(lambda x: str(x))
+    """Create cell id lane id """
+    df['date'] = df['date'].apply(lambda x:'c'+str(x))
     df['cell'] = df['date']+df['pos'].apply(lambda x: str(x))+df['gl'].apply(lambda x: str(x))+df['id'].apply(lambda x: str(x))
     df['lane_ID'] = df['date']+df['pos'].apply(lambda x: str(x))+df['gl'].apply(lambda x: str(x))
     return df
@@ -928,11 +928,14 @@ def corr_par(df,elrat,par_deg='parent_cell'):
 def long_corr_in_data(df,leng,ngen):
     """Find corr over generations in dataframes"""
     pre=['']
-    for k in range(ngen-1):
-        df = genalogy(df,'{}parent_cell'.format(pre[-1]))
-        pre.append(pre[-1]+'g_')
-    elrat = df.groupby('cell').apply(lambda x:\
-                                     linregress(x['time_sec'],np.log(x['{}'.format(leng)])).slope)
+    if 'g_'*(ngen-1)+'parent_cell' not in df.columns:
+        for k in range(ngen-1):
+            df = genalogy(df,'{}parent_cell'.format(pre[-1]))
+            pre.append(pre[-1]+'g_')
+    else:
+        for k in range(ngen-1):
+            pre.append(pre[-1]+'g_')
+    elrat = df.groupby('cell').apply(lambda x:linregress(x['time_sec'],np.log(x['{}'.format(leng)])).slope)
     pre = [k+'parent_cell' for k in pre]
     return np.vstack([corr_par(df,elrat,par_deg=k) for k in pre]),elrat.std()/elrat.mean()
 #def smilar_frame(W,cvd=0):
@@ -1018,6 +1021,8 @@ def adder(V0,W,DV,prin=False,dt_sim=1):
     x0=V0
     integ = []
     j=0
+    gr=[]
+    last=W[0]
     while True:
         W = np.insert(W,0,0)
         try:
@@ -1030,27 +1035,83 @@ def adder(V0,W,DV,prin=False,dt_sim=1):
         #Randomly choose if take first smaller or first larger which statistfy cond
         ind += np.random.choice([0,1])
         integ.append(exp[:ind])
+        te = W[:ind]; te[0]=last
+        gr.append(te)
+        try: 
+            last = W[ind-1]
+        except IndexError:
+            break
         W= W[ind:]
         x0=integ[-1][-1]/2
     #return integ
     #to_print = np.array([k[-1] for k in integ])
     #if prin: print('added length:',np.mean(to_print),'+/-',np.std(to_print)/np.sqrt(len(to_print)))
-    return integ[:-1]
-
-def smilar_frame(mlam,gamma,sl2,sm2,V0,DV,shape=(1,1000),dtsim=1,prin=False):
+    return integ[:-1],gr[:-1]
+def same_adder_shape(f,X):
+    """Give to f numpy array the same shape as X list of arrays"""
+    assert f.ndim==1
+    ind  = np.append(np.array([0]),np.cumsum([x.shape[0] for x in X]))
+    Y = []
+    for i in range(len(ind)-1):
+        Y.append(f[ind[i]:ind[i+1]])
+    return Y[:-1],X[:-1]
+def gfp_dynamics_1cc(F,V,Cm1,beta,dtsim):
+    """Knowing the concentration before division Cm1, mRNA dynamics F and Volume dynamics V compute GFP dynamics. Condition is concentration stay constant at division"""
+    G = np.zeros_like(F)
+    G[0] = Cm1*V[0]
+    for k in range(len(F)-1):
+        G[k+1] = G[k]+(V[k]*F[k]-beta*G[k])*dtsim
+    return G,G[-1]/V[-1]
+def gfp_dynamics(F,V,Cm1,beta,dtsim):
+    """Apply dynamics on all cells"""
+    G = []
+    n=0
+    for f,v in zip(F,V):
+        if n==0: cm1 = Cm1
+        g,cm1 = gfp_dynamics_1cc(f,v,cm1,beta,dtsim)
+        G.append(g)
+        n+=1
+    return G
+def the_pred_values(ml,gl,sl2,mq,gq,sq2,DV,beta):
+    """Easy values to comptue theoretically"""
+    print("Corr time in elongation rate = {} and in mRNA = {}".format(1/gl,1/gq))
+    print("Mean volume = {} and mean GFP = {}".format(3/2*DV,3/2*DV*mq/ml))
+    cvs = lambda x,y,z: np.sqrt(x/(2*y*z**2))
+    print("Cv in elongation rate {} and mRNA {}".format(cvs(sl2,gl,ml),cvs(sq2,gq,mq)))
+def\
+similar_frame(ml,gl,sl2,sm2,mq,gq,sq2,DV,beta,C0,shape=(1,1000),dtsim=1,gfp_sym=False):
     """From  OU create a dataframe with same shape as biological data (divison at twice the size).. Consider also asymmetric division """
+    if shape[0]>10: print('change naming of indexing cells since they may mix')
     explen = []; lane_ID=[]; parent_ID=[]; id_n=[-1]; time_sec=[]; df=[]
-    W = ornstein_uhlenbeck(mlam,gamma,sl2,shape,dtsim) # create elongation rate dynamics
+    W = ornstein_uhlenbeck(ml,gl,sl2,shape,dtsim) # create elongation rate dynamics
+    if gfp_sym :F = ornstein_uhlenbeck(mq,gq,sq2,shape,dtsim) # create elongation rate dynamics
     V = np.zeros_like(W)
-    V[:,0] = V0
+    V0=DV
+    V[:,0] = V0 
     for i in range(W.shape[0]):
-        X = adder(V0,W[i,:],DV,prin,dtsim)
+        X,Ww = adder(V0,W[i,:],DV,False,dtsim)#Adder model for size
+        if gfp_sym:
+            f,X = same_adder_shape(F[i,:],X)
+            Ww = Ww[:-1]
+            G = gfp_dynamics(f,X,C0,beta,dtsim)
+        else:
+            X = X[:-2]; Ww=Ww[:-2]
+            assert(sum([len(x)-len(w) for x,w in zip(X,Ww)])==0)
         for j,k in enumerate(X):
+            #if j<5: continue # discard first cells for stationarity
             lane_ID = ['{}'.format(i)]*int(len(k))
             parent_ID = ['{}'.format(id_n[-1])]*int(len(k))
-            id_n = ['{}'.format(j)]*int(len(k))
+            id_n = ['{}'.format(float(j))]*int(len(k))
             explen= np.exp(np.random.normal(np.log(k),np.sqrt(sm2)))
-            df.append(pd.DataFrame({'leng':explen,'lane_ID':lane_ID,'parent_id':parent_ID, 'id':id_n}))
+            if gfp_sym:
+                gfp = np.random.normal(G[j],np.sqrt(G[j]))
+                df.append(pd.DataFrame({'leng':explen,'lane_ID':lane_ID,'parent_id':parent_ID,\
+                                        'id':id_n,'gfp':gfp,'leng_no_noise':k,'growth_rate':Ww[j],'q_dyn':f[j]}))
+            else:
+                #print("ok")
+#                print(len(explen)-len(Ww[j]))
+                df.append(pd.DataFrame({'leng':explen,'lane_ID':lane_ID,'parent_id':parent_ID,\
+                                        'id':id_n,'leng_no_noise':k,'growth_rate':Ww[j]}))
     df = pd.concat(df,ignore_index=True)
     df['time_sec'] = np.arange(0,df.shape[0]*dtsim,dtsim)*60
     df['cell'] = df['lane_ID']+df['id']
